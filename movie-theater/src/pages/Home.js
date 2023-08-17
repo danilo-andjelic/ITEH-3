ae936044cc80eddf8f110fadac736ccadc7226bf
Danilo Andjelic <daniloandjelici9@gmail.com>
2023-08-17 14:59:14 +0200

    Add filter

--- movie-theater/src/pages/Home.js
+++ movie-theater/src/pages/Home.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MovieCard from '../components/MovieCard';
 import '../styles.css'; 
 
 function Home() {
 
   const movies = [
-    { title: 'Movie A', genre: 'Action', releaseYear: 2020 },
-    { title: 'Movie B', genre: 'Comedy', releaseYear: 2019 },
-    { title: 'Movie C', genre: 'Drama', releaseYear: 2021 },
+    { title: 'Avatar', genre: 'Action', releaseYear: 2020 },
+    { title: 'Barbie', genre: 'Comedy', releaseYear: 2019 },
+    { title: 'Before sunrise', genre: 'Drama', releaseYear: 2021 },
   ];
+
+  const [filterValue, setFilterValue] = useState('');
+  const filteredMovies = movies.filter(movie =>
+    movie.title.toLowerCase().includes(filterValue.toLowerCase())
+  );
 
   return (
     <div className="container">
       <h1>Welcome to the Movie Theater Website</h1>
       <h2>Featured Movies</h2>
+
+      <input
+        type="text"
+        placeholder="Filter by title"
+        value={filterValue}
+        onChange={e => setFilterValue(e.target.value)}
+      />
+
       <div className="movie-list">
-        {movies.map((movie, index) => (
+        {filteredMovies.map((movie, index) => (
           <MovieCard
             key={index}
             title={movie.title}
